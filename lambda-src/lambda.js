6f8a6007b1bd810dac2802bfab89250c1057c8b5
Michael Wittig <michael@widdix.de>
2019-10-20 12:14:34 +0200

    handle NoSuchKey error

--- lambda-src/lambda.js
+++ lambda-src/lambda.js
@@ -30,62 +30,72 @@ exports.handler = async function(event) {
   const s3 = new AWS.S3({apiVersion: '2006-03-01'});
   const cf = event.Records[0].cf;
   if ('s3' in cf.request.origin) {
-    const bucket = cf.request.origin.s3.domainName.split('.').slice(0, -3).join('.');
-    const key = `${cf.request.origin.s3.path}${cf.request.uri.substr(1)}`;
-    if (key.endsWith('.png')) {
-      const raw = await s3.getObject({
-        Bucket: bucket,
-        Key: key
-      }).promise().then(data => data.Body);
-      const optimized = await sharp(raw)
-        .png({
-          quality: 80
-        })
-        .toBuffer();
-      return imageResponse(cf, optimized, 'image/png');
-    }
-    if(key.endsWith('.jpg') || cf.request.uri.endsWith('.jpeg')) {
-      const raw = await s3.getObject({
-        Bucket: bucket,
-        Key: key
-      }).promise().then(data => data.Body);
-      const optimized = await sharp(raw)
-        .jpeg({
-          quality: 80
-        })
-        .toBuffer();
-      return imageResponse(cf, optimized, 'image/jpeg');
-    }
-    if(key.endsWith('.webp')) {
-      const keyWithoutExtension = key.slice(0, -4);
-      const raw = await s3.getObject({
-        Bucket: bucket,
-        Key: keyWithoutExtension + 'png'
-      }).promise().then(data => data.Body)
-        .catch(err => {
-          if (err.code === 'NoSuchKey') {
-            return s3.getObject({
-              Bucket: bucket,
-              Key: keyWithoutExtension + 'jpg'
-            }).promise().then(data => data.Body);
-          }
-          return Promise.reject(err);
-        })
-        .catch(err => {
-          if (err.code === 'NoSuchKey') {
-            return s3.getObject({
-              Bucket: bucket,
-              Key: keyWithoutExtension + 'jpeg'
-            }).promise().then(data => data.Body);
-          }
-          return Promise.reject(err);
-        });
-      const optimized = await sharp(raw)
-        .webp({
-          quality: 80
-        })
-        .toBuffer();
-      return imageResponse(cf, optimized, 'image/webp');
+    try {
+      const bucket = cf.request.origin.s3.domainName.split('.').slice(0, -3).join('.');
+      const key = `${cf.request.origin.s3.path}${cf.request.uri.substr(1)}`;
+      if (key.endsWith('.png')) {
+        const raw = await s3.getObject({
+          Bucket: bucket,
+          Key: key
+        }).promise().then(data => data.Body);
+        const optimized = await sharp(raw)
+          .png({
+            quality: 80
+          })
+          .toBuffer();
+        return imageResponse(cf, optimized, 'image/png');
+      }
+      if(key.endsWith('.jpg') || cf.request.uri.endsWith('.jpeg')) {
+        const raw = await s3.getObject({
+          Bucket: bucket,
+          Key: key
+        }).promise().then(data => data.Body);
+        const optimized = await sharp(raw)
+          .jpeg({
+            quality: 80
+          })
+          .toBuffer();
+        return imageResponse(cf, optimized, 'image/jpeg');
+      }
+      if(key.endsWith('.webp')) {
+        const keyWithoutExtension = key.slice(0, -4);
+        const raw = await s3.getObject({
+          Bucket: bucket,
+          Key: keyWithoutExtension + 'png'
+        }).promise().then(data => data.Body)
+          .catch(err => {
+            if (err.code === 'NoSuchKey') {
+              return s3.getObject({
+                Bucket: bucket,
+                Key: keyWithoutExtension + 'jpg'
+              }).promise().then(data => data.Body);
+            }
+            return Promise.reject(err);
+          })
+          .catch(err => {
+            if (err.code === 'NoSuchKey') {
+              return s3.getObject({
+                Bucket: bucket,
+                Key: keyWithoutExtension + 'jpeg'
+              }).promise().then(data => data.Body);
+            }
+            return Promise.reject(err);
+          });
+        const optimized = await sharp(raw)
+          .webp({
+            quality: 80
+          })
+          .toBuffer();
+        return imageResponse(cf, optimized, 'image/webp');
+      }
+    } catch (err) {
+      if (err.code === 'NoSuchKey') {
+        return {
+          status: '404',
+          statusDescription: 'Not Found'
+        };
+      }
+      throw err;
     }
   }
   return cf.request;
